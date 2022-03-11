2d1e388a206d3b6b898cc1dd13c5dabad276f8c2
ilias jabri <expert.ilyasse@outlook.com>
2022-03-11 13:40:38 +0100

    advance it more;

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 const body = document.body;
-body.style.margin = '0'
+body.style = 'margin:0; font-family: monospace;'
 
 function appendHeadTagElements()
 {
@@ -21,7 +21,7 @@ function appendHeadTagElements()
         
 }
 appendHeadTagElements();
-function appendBodyElements()
+function appendHeaderElements()
 {
     let header = document.createElement('header')
     let div1 = document.createElement('div');
@@ -31,33 +31,71 @@ function appendBodyElements()
     div1.setAttribute('id', 'toggles');
     div2.setAttribute('id' ,'sort-types')
 
-    div1.style = 'padding: 0px 10px; display:flex; flex: 1 1 200px; align-items: center; background-color: green; height: 100%; min-width: 200px; max-width: 600px;';
-    div2.style = 'display:flex; flex: 1 1 200px; align-items: center; background-color: red; height: 100%; min-width: 200px; max-width: 600px;';
+    div1.style = 'padding: 0px 10px; display:flex; flex: 1 1 200px; align-items: center; background-color: transparent; height: 100%; min-width: 200px; max-width: 600px;';
+    div2.style = 'display:flex; flex: 1 1 200px; align-items: center; background-color: transparent; height: 100%; min-width: 200px; max-width: 600px;';
     header.setAttribute('id', 'header');
-    header.style = 'width: 100%; height: 60px; background-color: blue; display:flex;  align-items: center; justify-content:center;';
+    header.style = 'width: 100%; height: 60px; background-color: #333; display:flex;  align-items: center; justify-content:center;';
 
     header.append(div1,div2);
     document.body.appendChild(header);
-    
 }
-appendBodyElements()
+appendHeaderElements();
 function advanceTogglesDiv()
 {
     let toggles = document.querySelector('#toggles');
-    toggles.style.backgroundColor = 'yellow';
-
+    let sortSection = document.querySelector('#sort-types')
 
     let randomArrayBtn = document.createElement('button')
     randomArrayBtn.setAttribute('id', 'random-btn')
     randomArrayBtn.innerText = 'randomize'
+    randomArrayBtn.style = 'color: white; background-color: #4455ff; border: none; padding: 7px;'
 
     let toggleInputTitle = document.createElement('span')
     toggleInputTitle.innerText = 'array length:'
+    toggleInputTitle.style = 'color: white; font-size: 14px;'
 
     let toggleInput = document.createElement('input')
     toggleInput.setAttribute('type', 'range')
     toggles.append(toggleInputTitle, toggleInput, randomArrayBtn)
 }
-advanceTogglesDiv()
+advanceTogglesDiv();
+
+function generateArrayScene()
+{
+    let sceneSection = document.createElement('section');
+    sceneSection.setAttribute('id','scene-section');
+    sceneSection.style = 'margin-top: 20px; display: flex;justify-content: center; width: 100%;height: fit-content;'
+
+    let scene = document.createElement('div');
+    scene.setAttribute('id', 'scene');
+    scene.style = 'margin: 0px 5px; display: flex; justify-content:center;gap:2px; flex: 1 1 100px; min-width: 100px; max-width: 2000px; height: 400px; background-color: black;';
+
+    sceneSection.appendChild(scene);
+    body.appendChild(sceneSection);
+}
+generateArrayScene()
+function generateArrayEntities(num){
+    let arrayScene = document.querySelector('#scene');
+    let array = [];
+    let maxHeight = 400;
 
 
+    for (let i = 0; i < num; i++) {
+        array.push((Math.random()*100));
+    }
+    array.forEach(element => {
+        // variables to make code more clear ;
+
+        let createdDiv = document.createElement('div');
+        let normal = element/100;
+        let divHeight = normal*maxHeight;
+
+        createdDiv.style = `background-color: blue; flex: 1 1 20px; height:${divHeight}px ;max-width: 30px;`;
+        arrayScene.appendChild(createdDiv)
+
+
+    })
+
+}
+
+generateArrayEntities(16)
